341e07cb0c05a2f16a5edb1244d7eb8314c1d90d
Divyansh Thakur <divyansh2140@gmail.com>
2019-06-30 15:15:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/uberclone/MainActivity.java
@@ -0,0 +1,155 @@
+package com.example.uberclone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    public void onClick(View view) {
+        if (edtDriverOrPassenger.getText().toString().equals("Driver") || edtDriverOrPassenger.getText().toString().equals("Passenger")) {
+            if (ParseUser.getCurrentUser() == null) {
+                ParseAnonymousUtils.logIn(new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null && e == null) {
+                            Toast.makeText(MainActivity.this,"Welcome user",Toast.LENGTH_SHORT).show();
+
+                            user.put("as", edtDriverOrPassenger.getText().toString());
+                            user.saveInBackground();
+                        }
+                    }
+                });
+            }
+        } else {
+            Toast.makeText(MainActivity.this,"Are you a Passenger or Driver?",Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    enum State {
+        SIGNUP, LOGIN
+    }
+
+    private State state;
+    private Button btnSignUp, btnOTLogIn;
+    private EditText edtUsername, edtPassword, edtDriverOrPassenger;
+    private RadioButton passengerRadioButton, driverRadioButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ParseUser.getCurrentUser() != null) {
+            // Intent activity
+        }
+
+        // Save the current Installation to Back4App
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        state = State.SIGNUP;
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnOTLogIn = findViewById(R.id.btnOTLogIn);
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        edtDriverOrPassenger = findViewById(R.id.edtDorP);
+        passengerRadioButton = findViewById(R.id.rdbPassenger);
+        driverRadioButton = findViewById(R.id.rdbDriver);
+
+
+        btnOTLogIn.setOnClickListener(this);
+
+
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (state == State.SIGNUP) {
+
+                    if (driverRadioButton.isChecked() == false || passengerRadioButton.isChecked() == false) {
+                        Toast.makeText(MainActivity.this,"Are you a Passenger or Driver?",Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    ParseUser appUser = new ParseUser();
+                    appUser.setUsername(edtUsername.getText().toString());
+                    appUser.setPassword(edtPassword.getText().toString());
+
+                    if (driverRadioButton.isChecked()) {
+                        appUser.put("as", "Driver");
+                    } else if (passengerRadioButton.isChecked()) {
+                        appUser.put("as", "Passenger");
+                    }
+
+                    appUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                Toast.makeText(MainActivity.this, "Signed Up!",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                } else if (state == State.LOGIN) {
+                    ParseUser.logInInBackground(edtUsername.getText().toString(), edtPassword.getText().toString(), new LogInCallback() {
+                        @Override
+                        public void done(ParseUser user, ParseException e) {
+                            if (user != null && e == null) {
+                                Toast.makeText(MainActivity.this,"User Logged in",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_sign_up_activity,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.loginItem :
+
+                if (state == State.SIGNUP) {
+                    state = State.LOGIN;
+                    btnSignUp.setText("Log In");
+                    item.setTitle("Sign Up");
+                } else if (state == State.LOGIN) {
+                    state = State.SIGNUP;
+                    btnSignUp.setText("Sign Up");
+                    item.setTitle("Log In");
+                }
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
